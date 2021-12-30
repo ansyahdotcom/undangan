57b785be1341be366a051d507ac753156f7b97f5
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2021-12-30 13:59:27 +0700

    tema

---
+++ app/Views/rsvpform/formbestdayblue.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="/assets/bestday/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/bestday/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="/assets/bestday/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/assets/bestday/css/flaticon/flaticon.css" rel="stylesheet">
+    <link href="/assets/bestday/css/owl.carousel.css" rel="stylesheet">
+    <link href="/assets/bestday/css/owl.transitions.css" rel="stylesheet">
+    <link href="/assets/bestday/css/colorbox.css" rel="stylesheet">
+    <link href="/assets/bestday/css/style.css" rel="stylesheet">
+    <link href="/assets/bestday/css/blue.css" rel="stylesheet">
+</head>
+
+<body>
+    <form action="transaksi/bestdayblue" method="post">
+        <div class="txt_input">
+            <input type="text" class="form-control" id="name_block_1" placeholder="Nama anda" style="text-align: center;">
+        </div>
+        <div class="txt_input wa">
+            <input type="text" class="form-control wa" id="email_block_1" placeholder="Nomor WA" style="text-align: center;">
+        </div>
+        <div class="txt_input">
+            <input type="text" class="form-control" id="guest_block_1" placeholder="Jumlah yang hadir" style="text-align: center;">
+        </div>
+        <div class="txt_input">
+            <select name="kehadiran" id="kehadiran" class="form-control" style="text-align: center;">
+                <option value="1">Hadir</option>
+                <option value="2">Tidak hadir</option>
+            </select>
+        </div>
+        <div class="row">
+            <button type="submit" class="btn btn-lg submit_block_1">Send</button>
+        </div>
+    </form>
+
+    <script src="/assets/bestday/js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="/assets/bestday/js/jquery.plugin.min.js"></script>
+    <script type="text/javascript" src="/assets/bestday/js/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="/assets/bestday/js/jquery.onepage-scroll.min.js"></script>
+    <script src="/assets/bestday/js/jquery.colorbox-min.js"></script>
+    <script src="/assets/bestday/js/owl.carousel.min.js"></script>
+    <script src="/assets/bestday/js/bootstrap.min.js"></script>
+    <script src="/assets/bestday/js/skrollr.min.js"></script>
+    <script src="/assets/bestday/js/prefixfree.min.js"></script>
+    <script src="/assets/bestday/js/jquery.firefly-0.3-min.js"></script>
+    <script src="/assets/bestday/js/script.js"></script>
+</body>
+
+</html>
